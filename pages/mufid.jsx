3d878f8ec9c53604516bd2fa23e5e04c8ede4b70
mufidmoesz <mufidmusyaffa@365.telkomuniversity.ac.id>
2022-04-04 00:00:49 +0700

    mufid

--- pages/mufid.jsx
+++ pages/mufid.jsx
@@ -1,7 +1,10 @@
-export default mufid =() => {
+import styles from '../styles/mufid.module.css';
+
+
+export default function mufid (){
     return(
         <div>
-            <h1>Mufid Musyaffa</h1>
+            <h1 className={styles.h1}>Mufid Musyaffa</h1>
         </div>
     )
 }
