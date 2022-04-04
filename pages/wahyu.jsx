e2611149d440a432ddf70f3c3531c18ccc4b3284
Cicak Kayang <eriscapricron@gmail.com>
2022-04-04 12:38:48 +0700

    main

--- pages/wahyu.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/wahyu.module.css'
-
-export default function Wahyu(){
-    return(
-        <div className={styles.wahyu}>
-            <h1>Hello Wahyuu</h1>
-        </div>
-    )
-}
