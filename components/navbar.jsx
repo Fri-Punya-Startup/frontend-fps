b5896c68a1c56af1a596d8ffe79329f76ac9fa8c
Cicak Kayang <eriscapricron@gmail.com>
2022-04-07 03:34:01 +0700

    wahyu

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Navbar(){
     return (
         <React.Fragment>
-            
+
+
         </React.Fragment>
     )       
 }
