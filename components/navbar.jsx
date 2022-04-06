c306cd2b0042c1caec85f387d1844bd6eb44d1fc
Cicak Kayang <eriscapricron@gmail.com>
2022-04-07 03:44:42 +0700

    home

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navbar(){
-    return (
+    return (  
         <React.Fragment>
-
-
         </React.Fragment>
     )       
 }
